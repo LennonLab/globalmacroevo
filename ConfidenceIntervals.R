8b89bc8cab25dea7844ef82b4e96771f2c00c7f8
fordfishman <fordfishman@gmail.com>
2019-12-10 17:29:19 -0500

    Changed name of ProbabilityGeneration.R to PMF.R and added in documentation

--- ConfidenceIntervals.R
+++ ConfidenceIntervals.R
@@ -1,4 +1,10 @@
-# Confidence intervals
+## Ford Fishman
+## December 10, 2019
+
+## Construct richness confidence intervals for stochastic birth-death processes
+## with different sets of parameter values (speciation, extinction rates).
+
+## SETUP ENVIRONMENT
 rm(list=ls())
 wd <- "~/Documents/LennonLab/GitHub/MicroSpeciation"
 data_dir <- paste(wd, "/data/", sep = "")
@@ -9,14 +15,14 @@ require(R.utils, quietly = T)
 require(ggplot2, quietly = T)
 require(viridis, quietly = T)
 
-sims <- 100000
+sims <- 100000 # some arbitrary amount of simulations
 lambda <- runif(n = sims, min = 0.007, max = 0.031) # speciation rates
 epsilon <- runif(n = sims, min = 0.0, max = 0.9) # mu/lambda
 mu <- lambda * epsilon # extinction rates
 r <- lambda - mu # diversification rate
 Time <- 4000 # total time to present 
 ES <- exp(r*Time) # expected richness
-
+k <- 0 # value to test tail functions against
 # subset to constrain complexity
 cutoff <- 10^16
 lambda <- lambda[ES<cutoff]
@@ -24,32 +30,38 @@ epsilon <- epsilon[ES<cutoff]
 mu <- mu[ES<cutoff]
 r <- r[ES<cutoff]
 
-k <- 0
-# from Magallon et al 2002
-beta <- (exp(r*Time)-1)/(exp(r*Time) - mu/lambda)
+# from Bailey (1964; eq. 8.46)
+beta <- lambda*(exp(r*Time)-1)/(lambda*exp(r*Time) - mu)
 
+# Finds richness confidence intervals for given parameters
 CIs <- function(){
-  timesup <- F
-  ptm <- proc.time()[3]
-  while (!timesup&(RoundingLowAll|RoundingHighAll)) {
-    k <- runif(n=1,min=0,max=10) * 10^runif(n=1,min=0,max=22)
-
-    if (RoundingLowAll&RoundingHighAll){
+  timesup <- F # is time limit up?
+  ptm <- proc.time()[3] # start recording time
+  while (!timesup&(RoundingLowAll|RoundingHighAll)) { # if time isn't up and CI's aren't all found yet
+    # create a random richness within a feasible range for the richness estimates
+    k <- runif(n=1,min=0,max=10) * 10^runif(n=1,min=0,max=22) 
+    if (RoundingLowAll&RoundingHighAll){ # if some parameter set doesn't have full CI
       # probability that richness at a given time is greater than k
       P_greater <- beta^(k-1)
       # probability that richness at a given time is less than k
       P_lower <- 1 - beta^(k-1)
-      atLowerBound <- round(P_greater, digits = 3)==0.975
+      # if prob. is 0.975 that process should produce a richness greater than k
+      atLowerBound <- round(P_greater, digits = 3)==0.975 
+      # if prob. is 0.975 that process should produce a richness less than k
       atUpperBound <- round(P_lower, digits = 3)==0.975
-      RoundingLow <- ifelse(atLowerBound|!RoundingLow==F,F,T)
-      RoundingHigh <- ifelse(atUpperBound|!RoundingHigh==F,F,T)
-      klow <- ifelse(atLowerBound,k,klow)
-      khigh <- ifelse(atUpperBound,k,khigh)
-    } else if(RoundingHighAll){
+      RoundingLow <- ifelse(atLowerBound|!RoundingLow==F,F,T) # is lower bound found yet
+      RoundingHigh <- ifelse(atUpperBound|!RoundingHigh==F,F,T) # is upper bound found yet
+      klow <- ifelse(atLowerBound,k,klow) # if yes to above, set lower bound
+      khigh <- ifelse(atUpperBound,k,khigh) # same for upper bound
+      
+      # if only need lower bounds
+    } else if(RoundingHighAll){ 
       P_lower <- 1 - beta^(k-1)
       atUpperBound <- round(P_lower, digits = 3)==0.975
       RoundingHigh <- ifelse(atUpperBound|!RoundingHigh==F,F,T)
       khigh <- ifelse(atUpperBound,k,khigh)
+      
+      # if only need upper bounds
     } else if(RoundingLowAll){
       P_greater <- beta^(k-1)
       atLowerBound <- round(P_greater, digits = 3)==0.975
@@ -58,24 +70,24 @@ CIs <- function(){
     }
     if (! NA %in% klow) {RoundingLowAll <- F}
     if (! NA %in% khigh) {RoundingHighAll <- F}
-    if (proc.time()[3]-ptm>60*5) {timesup <- T} # dont run process for more than 10s 
+    if (proc.time()[3]-ptm>60*5) {timesup <- T} # don't run process for more than 10s 
   }
-  # kvals <- list(klow=klow, khigh=khigh)
-  return(list(klow=klow, khigh=khigh))
+
+  return(list(klow=klow, khigh=khigh)) # return bounds as a list of 2 vectors
 }
-# data_m <- matrix(0,nrow=sims,ncol=5)
-newSims <- length(r)
-klow <- rep(NA, newSims)
+
+newSims <- length(r) # after controlling for numbers that are too large, how many simulations are run
+klow <- rep(NA, newSims) # pre-vectorize with NAs
 khigh <- rep(NA, newSims)
 
 RoundingLow = !logical(newSims) # has the lower boundary been found? a vector of booleans (TRUE)
 RoundingHigh = !logical(newSims) # has the upper boundary been found?
-RoundingLowAll = TRUE
+RoundingLowAll = TRUE 
 RoundingHighAll = TRUE
 
-ptm <- proc.time()
-klist <- CIs()
-proc.time() - ptm
+ptm <- proc.time() # start time
+klist <- CIs() # run main process
+proc.time() - ptm # check process run time#
 klows <- klist[[1]]
 khighs <- klist[[2]]
 newSims
@@ -84,21 +96,24 @@ newSims
 # hasData <- !is.na(klows)&!is.na(khighs)
 # klows <- klows[hasData]
 
-
+# create df for plot
 df <- data.frame(lambda, epsilon, mu, r, klows, khighs)
 
+# Locey & Lennon (2016) interval
 low12 <- 0.385*10^12
 high12 <- 3.43*10^12
-# low12 <- 0.00001*(10^12)
-# high12 <- 1000*(10^12)
+# Louca et al. 2019 interval
 low6 <- 2.2*10^6
 high6 <- 4.3*10^6
 
+# is there overlap between the CIs and above intervals?
 df$CI12 <- !(klows<low12 & khighs<low12) & !(klows>high12 & khighs>high12) 
 df$CI6 <- !(klows<low6 & khighs<low6) & !(klows>high6 & khighs>high6) 
 
-df1 <- subset(df, !is.na(CI6))
-df1$CI <- ifelse(df1$CI12,"CI12",ifelse(df1$CI6,"CI6","none"))
+df1 <- subset(df, !is.na(CI6)) # remove NAs
+df1$CI <- ifelse(df1$CI12,"CI12",ifelse(df1$CI6,"CI6","none")) # categorize simulations
+
+# plot figure
 p1 <- ggplot(data = df1, aes(x = lambda, y = epsilon, color = CI)) + 
   geom_point(size = 1.4) +
   xlab(expression(lambda*" (Species/Myr)")) +
--- code/PMF.R
+++ code/PMF.R
@@ -1,9 +1,17 @@
 ## Ford Fishman
-## October 22, 2019
+## December 10, 2019
 
-## Use a probability generating function to give probability of seeing a certain microbial
+## Use a probability mass function to give probability of seeing certain levels of microbial 
 ## richness giving a set of parameters.
-## Assumes stochastic birth-death process starting with 1 species. 
+#
+## Assumes stochastic birth-death process starting with 1 species.
+#
+## Traditionally, probabilities from a PMF are summed over a desired range of discrete outcomes to 
+## calculate the probability of outcomes from that range occuring. However, given the scale of the
+## ranges we are considering, vector lengths exceed 10^12, so the integrate() function is used in a
+## similar fashion to how one would approach probability calculation for a proability density function
+## (PDF). Testing showed that even at the scales of 10^6, integration proved to give similar outcomes 
+## to summation. 
 
 ## SETUP ENVIRONMENT
 rm(list=ls()) # removes all objects in the given environment
@@ -12,35 +20,37 @@ data_dir <- paste(wd, "/data/", sep = "")
 figure_dir <- paste(wd, "/figures/", sep = "")
 getwd()
 setwd(wd)
-require(ggplot2)
-require(viridis)
-require(R.utils)
+require(ggplot2, quietly = T)
+require(viridis, quietly = T)
 
 ## define parameters alpha and beta
 sims = 400000 # number of simulations
 lambda <- runif(n = sims, min = 0.001, max = 0.031) # speciation rates
-epsilon <- runif(n = sims, min = 0.1, max = 0.9) # mu/lambda
+epsilon <- runif(n = sims, min = 0.0, max = 0.9) # mu/lambda
 mu <- lambda * epsilon # extinction rates
 r <- lambda - mu # diversification rate
-t <- 1
-Time <- 4000 # total time to present 
-# alpha and beta are terms in the probability generating function
-alpha <- (mu*(exp(r*Time)-1))/(lambda*exp(r*Time)-mu)
-beta <- alpha*(lambda/mu)
+Time <- 4000 # total time to present
 
-## density function
-# func <- function(S) (1-alpha)*(1-beta)*beta^(S-1)
-## probability generating function
-pgf <- function(Smin, Smax){
-  prob <- double(length(lambda))
-  for (i in 1:length(lambda)){
-    func <- function(S) (1-alpha[i])*(1-beta[i])*beta[i]^(S-1)
-    prob[i] <- integrate(func, Smin, Smax)$value
+# alpha and beta are terms in the probability generating function
+alpha <- (mu*(exp(r*Time)-1))/(lambda*exp(r*Time)-mu) # probability of extinction 
+beta <- alpha*(lambda/mu) # modifier of alpha
+
+
+## probability mass function
+pmf_integration <- function(Smin, Smax){
+  # make vector in advanced to reduce runtime
+  prob <- double(length(lambda)) 
+  # need to use for loop, integrate() doesn't like vectors
+  for (i in 1:length(lambda)){ 
+    # This pmf is taken from Raup (1985; Eq. A17), which is in turn from Bailey (1964; eq 8.46)
+    pmf <- function(S) (1-alpha[i])*(1-beta[i])*beta[i]^(S-1) 
+    # integrating overall interval of given richness estimation
+    prob[i] <- integrate(pmf, Smin, Smax)$value
   }
   return(prob)
 }
 
-## plot relative, normalized, or unaltered probabilities
+## plot probabilities 
 probPlot <- function(df){
   return(ggplot(data = df, aes(x = lambda, y = epsilon, color = prob)) + 
     geom_point(size = 0.4) +
@@ -54,25 +64,25 @@ probPlot <- function(df){
           axis.line = element_blank()))
 }
 # Probability of having exactly 10^12 species
-dfS12 <- data.frame(lambda, epsilon, prob = pgf(0.385*10^12, 3.43*10^12))
+dfS12 <- data.frame(lambda, epsilon, prob = pmf_integration(0.385*10^12, 3.43*10^12))
 (pS12 <- probPlot(dfS12))
 # Save figure
 figS12_dir <- paste(figure_dir, "probS12_stochastic.png", sep = "")
 ggsave(plot = pS12, filename = figS12_dir, width = 7, height = 5)
 
 # Probability of being in range for Louca et al. 2019
-dfS6 <- data.frame(lambda, epsilon, prob = pgf(2.2*10^6, 4.3*10^6))
+dfS6 <- data.frame(lambda, epsilon, prob = pmf_integration(2.2*10^6, 4.3*10^6))
 (pS6 <- probPlot(dfS6))
 figS6_dir <- paste(figure_dir, "probS6_stochastic.png", sep = "")
 ggsave(plot = pS6, filename = figS6_dir, width = 7, height = 5)
 
-
+# Probabilities for both sets of estimates on same plot
 df6_12 <- data.frame(lambda, epsilon, prob = dfS12$prob + dfS6$prob)
 pS6_12 <- probPlot(df6_12)
 figS6_12_dir <- paste(figure_dir, "probS6_12_stochastic.png", sep = "")
 ggsave(plot = pS6_12, filename = figS6_12_dir, width = 7, height = 5)
 
-## variance
+## variance from Raup 1985 A22, via Bailey (1964; eq 8.49)
 variance <- (lambda + mu)/(lambda - mu) * exp((lambda - mu)*Time)*(exp((lambda - mu)*Time) - 1)
 expectS <- exp(r * Time)
 dfvar <- data.frame(lambda, epsilon, cov = (variance)^0.5/expectS)
@@ -91,5 +101,20 @@ dfvar <- data.frame(lambda, epsilon, cov = (variance)^0.5/expectS)
 var_fig_dir <- paste(figure_dir, "CoV_stochastic.png", sep = "")
 ggsave(plot = pvar, filename = var_fig_dir, width = 7, height = 5)
 
+## TESTING INTEGRATION VS SUMMING
+## it seems that for the scales we are dealing with, the probabilities aren't different enough
+## to worry that PMF's should be summed over integers and not integrated
 
+# lambda <- 0.05
+# epsilon <- 0.1
+# mu <- lambda*epsilon
+# r <- lambda - mu
+# Time <- 400
+# log10(exp(r*Time)) # check general scale of number
+# alpha <- (mu*(exp(r*Time)-1))/(lambda*exp(r*Time)-mu) # probability of extinction 
+# beta <- alpha*(lambda/mu) 
+# func <- function(S) (1-alpha)*(1-beta)*beta^(S-1) #pmf
+# Srange <- (0.385*10^6):(3.43*10^7) # a appropriate for the scale in question
+# integrate(func, 0.385*10^6, 3.43*10^7)$value # the integration-based probability
+# sum(func(Srange)) # the sum-based probability
 
