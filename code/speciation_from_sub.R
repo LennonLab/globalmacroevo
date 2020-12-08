8f633fc6657f440c2a217c8709472ea1d6dcfe61
DESKTOP-6AF44U4\fordf <fordfishman@gmail.com>
2020-12-08 18:05:35 -0500

    Calculation of speciation rates from divergence rates

--- code/speciation_from_sub.R
+++ code/speciation_from_sub.R
@@ -1,13 +1,21 @@
+length.g <- 1600 # length of gene
+cutoff <- 0.03 # % divergence for new OTU
+subs <- length.g * cutoff # number of substitutions needed for new OTU
+
+# 16S substitutions per nucleotide per million years (Kuo and Ochman 2009)
 low.K16 <- 0.00013
 high.K16 <- 0.00091
 
-rna.length <- 200
-threepercent <- rna.length * 0.03
+# number of 16S substitutions per million years
+low.sub <- low.K16*length.g
+hi.sub <- high.K16*length.g
 
+# Corresponding divergence time (million years):
+div.t.ls <- subs / low.sub # divergence time w/ low substitution rate
+div.t.hs <- subs / hi.sub # divergence time w/ high substitution rate
 
-low.sub.num <- low.K16 * rna.length
-high.sub.num <- high.K16 * rna.length
-low.sub.num/threepercent
-high.sub.num/threepercent
-0.00013/0.03
-0.00091/0.03
+# Corresponding speciation rates (million years):
+spec.ls <- 1 / div.t.ls
+spec.hs <- 1 / div.t.hs
+
+cat("Speciation rates range from", round(spec.ls, 3), "to", round(spec.hs, 3), "species per million years") 
