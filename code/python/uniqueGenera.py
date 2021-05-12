cbd3d9b54db1d5b488a9f33b48786b15a59ace45
DESKTOP-6AF44U4\fordf <fordfishman@gmail.com>
2021-05-12 16:57:58 -0400

    Obtain unique genera

---
+++ code/python/uniqueGenera.py
@@ -0,0 +1,62 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--file', default='allseqs.out', type=str, help="Desired name of input path")
+arguments = parser.parse_args()
+filename = arguments.file
+
+species = dict()
+
+with open(filename) as r:
+    
+    for line in r:
+
+        line = line.strip()
+        parts = line.split('\t') 
+
+        query = parts[0]
+
+
+        if not query in results:
+
+            species[query] = [[parts[6]],[parts[3]]] #[[species],[perc. ident]]
+
+        else:
+            results[query][0].append(parts[6]) # species
+            results[query][1].append(parts[3]) # perc. ident
+
+final_genera = dict()
+
+for query, parts in species.items():
+    spp = parts[0]; ident = parts[1]
+    genera = [sp.split(" ")[0] for sp in spp] # take the genus
+
+    counts = dict()
+
+    for i in range(len(genera)):
+        genus = genera[i]
+        pident = ident[i]
+
+        if genus in counts: # scale results by percent identity
+            counts[genus] += pident
+
+        else:
+            counts[genus] = p_ident
+    
+    # pick the genus with the highest weighted count
+    genus = max(counts, key=counts.get)
+    final_genera[query] = genus
+
+with open("query_genera.txt", "w") as f:
+
+    for query, genus in final_genera.items():
+
+        f.write("%s\t%s\n" % (query, genus))
+
+unique_genera = set(final_genera.values())
+
+with open("unique_genera.txt", "w") as f:
+
+    for genus in unique_genera:
+        f.write("%s\n" % genus)
+    
