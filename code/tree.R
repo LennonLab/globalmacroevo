3fe74e31cc740128d2f6f291f57d44e9766824e3
fordfishman <fordfishman@gmail.com>
2021-09-28 16:05:32 -0400

    pairs tree

--- code/tree.R
+++ code/tree.R
@@ -2,22 +2,23 @@
 ## Ford Fishman
 ## 6/30/21
 
+library('adephylo')
 library('ape')
 library('phytools')
 
 
 
-ml.bootstrap <- read.tree("data/16S/RAxML_bipartitions.T1")
+ml.bootstrap <- read.tree("data/16S/tree/pairs/RAxML_bipartitions.T1")
 
 
-outgroup <- match( "MG642124.1", ml.bootstrap$tip.label)
-ml.rooted <- root(ml.bootstrap, outgroup, resolve.root = T)
+# outgroup <- match( "MG642124.1", ml.bootstrap$tip.label)
+# ml.rooted <- root(ml.bootstrap, outgroup, resolve.root = T)
 
-ml.rooted$edge.length <- NULL
-
+# ml.rooted$edge.length <- NULL
+dists <- distRoot(ml.bootstrap)
+print(dists[order(names(dists))])
 par(mar=c(1,1,2,1)+0.1)
-plotTree(ml.rooted,type="fan",fsize=0.7,lwd=1,
-         ftype="off",xlim=c(-1.1,1.1))
+plotTree(ml.bootstrap,type="fan",fsize=0.7,lwd=1,xlim=c(-1.1,1.1))
 # add.scale.bar(cex=0.7)
 # nodelabels(ml.bootstrap$node.label, font=2, bg="white", frame="r", cex=0.5)
 
